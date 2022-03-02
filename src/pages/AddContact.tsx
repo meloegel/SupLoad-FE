f9f293bd42a33eaf980d7352c3c958840792c710
meloegel <meloegel@gmail.com>
2022-03-01 22:45:03 -0500

    added bare bones add contact

---
+++ src/pages/AddContact.tsx
@@ -0,0 +1,7 @@
+export default function AddContact(): JSX.Element {
+  return (
+    <div>
+      <h2>Add Contact</h2>
+    </div>
+  );
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../account/Login";
 import Register from "../account/Register";
+import AddContact from "../pages/AddContact";
 import Home from "../pages/Home";
 
 export default function Router(): JSX.Element {
@@ -10,6 +11,7 @@ export default function Router(): JSX.Element {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/add-contact" element={<AddContact />} />
       </Routes>
     </BrowserRouter>
   );
