07f6ba9577b7cd09152a5a75d68fdffb33b5439f
meloegel <meloegel@gmail.com>
2022-03-05 18:41:43 -0500

    basic contact styling

--- src/common/Contact.tsx
+++ src/common/Contact.tsx
@@ -1,18 +1,20 @@
 export default function Contact(props: ContactType): JSX.Element {
   return (
-    <div className="w-1/2 m-auto text-lg">
-      <h2>Contact</h2>
+    <div className="w-2/6 m-auto text-xl bg-white p-4">
+      <h2 className="text-center text-2xl">Contact</h2>
       <h3>
         Name: {props.firstname} {props.lastname}
       </h3>
-      <div className="flex justify-evenly">
-        <h3>Email: {props.email}</h3>
-        <h3>Phone: {props.phone}</h3>
-      </div>
-      <div>
+      <h3>Email: {props.email}</h3>
+      <h3>Phone: {props.phone}</h3>
+      <div className="flex">
         <h3>Address: </h3>
-        <h4>{props.street}</h4>
-        <h4>{props.city} {props.state} {props.zip}</h4>
+        <div>
+          <h4>{props.street}</h4>
+          <h4>
+            {props.city} {props.state} {props.zip}
+          </h4>
+        </div>
       </div>
     </div>
   );
