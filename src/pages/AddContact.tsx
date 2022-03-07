a00325a7ef0a50c6ffbe8f411b883e7d38288a83
meloegel <meloegel@gmail.com>
2022-03-06 21:56:44 -0500

    added input to addContact

--- src/pages/AddContact.tsx
+++ src/pages/AddContact.tsx
@@ -18,7 +18,7 @@ const initialFormValues = {
 
 export default function AddContact(): JSX.Element {
   const [formValues, setFormValues] = useState(initialFormValues);
-   const [request, data, statusCode] = useFetch<any>();
+  const [request, data, statusCode] = useFetch<any>();
 
   const {
     register,
@@ -86,6 +86,111 @@ export default function AddContact(): JSX.Element {
             {errors.firstname && (
               <p className="text-red-600 text-xs m-2">
                 {errors.firstname?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>Last Name</label>
+            <input
+              {...register("lastname")}
+              value={formValues.lastname}
+              onChange={onInputChange}
+              name="lastname"
+              type="text"
+            />
+            {errors.lastname && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.lastname?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>Email</label>
+            <input
+              {...register("email")}
+              value={formValues.email}
+              onChange={onInputChange}
+              name="email"
+              type="text"
+            />
+            {errors.email && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>Street</label>
+            <input
+              {...register("street")}
+              value={formValues.street}
+              onChange={onInputChange}
+              name="street"
+              type="text"
+            />
+            {errors.street && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.street?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>City</label>
+            <input
+              {...register("city")}
+              value={formValues.city}
+              onChange={onInputChange}
+              name="city"
+              type="text"
+            />
+            {errors.city && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.city?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>State</label>
+            <input
+              {...register("state")}
+              value={formValues.state}
+              onChange={onInputChange}
+              name="state"
+              type="text"
+            />
+            {errors.state && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.state?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>Zip</label>
+            <input
+              {...register("zip")}
+              value={formValues.zip}
+              onChange={onInputChange}
+              name="zip"
+              type="text"
+            />
+            {errors.zip && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.zip?.message}
+              </p>
+            )}
+          </div>
+          <div className="p-2">
+            <label>Phone Number</label>
+            <input
+              {...register("phone")}
+              value={formValues.phone}
+              onChange={onInputChange}
+              name="phone"
+              type="text"
+            />
+            {errors.phone && (
+              <p className="text-red-600 text-xs m-2">
+                {errors.phone?.message}
               </p>
             )}
           </div>
