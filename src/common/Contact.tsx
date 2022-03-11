4c27e261c183c34ecab67d0b4346007c00249c45
meloegel <meloegel@gmail.com>
2022-03-10 22:40:12 -0500

    changed contact component to match add-contact

--- src/common/Contact.tsx
+++ src/common/Contact.tsx
@@ -2,18 +2,21 @@ export default function Contact(props: ContactType): JSX.Element {
   return (
     <div className="w-2/6 m-auto text-xl bg-white p-4">
       <h2 className="text-center text-2xl">Contact</h2>
-      <h3>
-        Name: {props.firstname} {props.lastname}
-      </h3>
-      <h3>Email: {props.email}</h3>
-      <h3>Phone: {props.phone}</h3>
+      <div className="flex">
+        <h3>Name: {props.firstname}</h3>
+        <h3>Last: {props.lastname}</h3>
+      </div>
+      <div className="flex">
+        <h3>Email: {props.email}</h3>
+        <h3>Phone: {props.phone}</h3>
+      </div>
       <div className="flex">
         <h3>Address: </h3>
         <div>
-          <h4>{props.street}</h4>
-          <h4>
-            {props.city} {props.state} {props.zip}
-          </h4>
+          <h4>Street: {props.street}</h4>
+          <h4>City: {props.city}</h4>
+          <h4>State: {props.state}</h4>
+          <h4>Zip: {props.zip}</h4>
         </div>
       </div>
     </div>
