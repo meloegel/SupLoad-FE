c49276c21e5a5de742e036b6c9c5e0ba5da6a97a
meloegel <meloegel@gmail.com>
2022-03-16 22:51:37 -0400

    coverted contact component to use a table

--- src/common/Contact.tsx
+++ src/common/Contact.tsx
@@ -2,24 +2,40 @@ export default function Contact(props: ContactType): JSX.Element {
   return (
     <div className="w-5/12 mx-auto m-2 text-xl bg-white p-4">
       <h2 className="text-center text-2xl">Contact</h2>
-      <div className="flex ">
-        <h3 className="px-4 py-.5">First: {props.firstname}</h3>
-        <h3 className="px-4 py-.5">Last: {props.lastname}</h3>
-      </div>
-      <div className="flex ">
-        <h3 className="px-4 py-.5">Email: {props.email}</h3>
-        <h3 className="px-4 py-.5">Phone: {props.phone}</h3>
-      </div>
-      <div className="flex">
-        <div>
-          <h4 className="px-4 py-.5">Street: {props.street}</h4>
-          <h4 className="px-4 py-.5">State: {props.state}</h4>
-        </div>
-        <div>
-          <h4 className="px-4 py-.5">City: {props.city}</h4>
-          <h4 className="px-4 py-.5">Zip: {props.zip}</h4>
-        </div>
-      </div>
+      <table className="m-auto">
+        <tr>
+          <td>
+            <h3 className="px-4 py-.5">First: {props.firstname}</h3>
+          </td>
+          <td>
+            <h3 className="px-4 py-.5">Last: {props.lastname}</h3>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h3 className="px-4 py-.5">Email: {props.email}</h3>
+          </td>
+          <td>
+            <h3 className="px-4 py-.5">Phone: {props.phone}</h3>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h4 className="px-4 py-.5">Street: {props.street}</h4>
+          </td>
+          <td>
+            <h4 className="px-4 py-.5">City: {props.city}</h4>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h4 className="px-4 py-.5">State: {props.state}</h4>
+          </td>
+          <td>
+            <h4 className="px-4 py-.5">Zip: {props.zip}</h4>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
