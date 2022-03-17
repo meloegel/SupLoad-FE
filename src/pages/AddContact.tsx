b0caf18a5d918c3020aa1919a84dc3827d874883
meloegel <meloegel@gmail.com>
2022-03-16 23:53:25 -0400

    updated add-contact to use table

--- src/pages/AddContact.tsx
+++ src/pages/AddContact.tsx
@@ -78,142 +78,160 @@ export default function AddContact(): JSX.Element {
         <div className="w-1/2 m-auto text-xl bg-white p-4">
           <form onSubmit={handleSubmit(onSubmit)}>
             <h2 className="text-center text-2xl">Contact</h2>
-            <div className="flex">
-              <div className="p-2">
-                <label>First Name</label>
-                <input
-                  {...register("firstname")}
-                  value={formValues.firstname}
-                  onChange={onInputChange}
-                  name="firstname"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.firstname && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.firstname?.message}
-                  </p>
-                )}
-              </div>
-              <div className="p-2">
-                <label>Last Name</label>
-                <input
-                  {...register("lastname")}
-                  value={formValues.lastname}
-                  onChange={onInputChange}
-                  name="lastname"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.lastname && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.lastname?.message}
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="flex">
-              <div className="p-2">
-                <label>Email</label>
-                <input
-                  {...register("email")}
-                  value={formValues.email}
-                  onChange={onInputChange}
-                  name="email"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.email && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.email?.message}
-                  </p>
-                )}
-              </div>
-              <div className="p-2">
-                <label>Phone Number</label>
-                <input
-                  {...register("phone")}
-                  value={formValues.phone}
-                  onChange={onInputChange}
-                  name="phone"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.phone && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.phone?.message}
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="flex">
-              <div className="p-2">
-                <label>Street</label>
-                <input
-                  {...register("street")}
-                  value={formValues.street}
-                  onChange={onInputChange}
-                  name="street"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2 "
-                />
-                {errors.street && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.street?.message}
-                  </p>
-                )}
-              </div>
-              <div className="p-2">
-                <label>City</label>
-                <input
-                  {...register("city")}
-                  value={formValues.city}
-                  onChange={onInputChange}
-                  name="city"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.city && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.city?.message}
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="flex">
-              <div className="p-2">
-                <label>State</label>
-                <input
-                  {...register("state")}
-                  value={formValues.state}
-                  onChange={onInputChange}
-                  name="state"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.state && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.state?.message}
-                  </p>
-                )}
-              </div>
-              <div className="p-2">
-                <label>Zip</label>
-                <input
-                  {...register("zip")}
-                  value={formValues.zip}
-                  onChange={onInputChange}
-                  name="zip"
-                  type="text"
-                  className="border-2 rounded p-1 mx-2"
-                />
-                {errors.zip && (
-                  <p className="text-red-600 text-xs m-2">
-                    {errors.zip?.message}
-                  </p>
-                )}
-              </div>
-            </div>
+            <table className="m-auto">
+              <tr>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">First Name</label>
+                    <input
+                      {...register("firstname")}
+                      value={formValues.firstname}
+                      onChange={onInputChange}
+                      name="firstname"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.firstname && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.firstname?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="p-2">
+                    <label>Last Name</label>
+                    <input
+                      {...register("lastname")}
+                      value={formValues.lastname}
+                      onChange={onInputChange}
+                      name="lastname"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.lastname && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.lastname?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">Email</label>
+                    <input
+                      {...register("email")}
+                      value={formValues.email}
+                      onChange={onInputChange}
+                      name="email"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.email && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.email?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">Phone</label>
+                    <input
+                      {...register("phone")}
+                      value={formValues.phone}
+                      onChange={onInputChange}
+                      name="phone"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.phone && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.phone?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">Street</label>
+                    <input
+                      {...register("street")}
+                      value={formValues.street}
+                      onChange={onInputChange}
+                      name="street"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2 "
+                    />
+                    {errors.street && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.street?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">City</label>
+                    <input
+                      {...register("city")}
+                      value={formValues.city}
+                      onChange={onInputChange}
+                      name="city"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.city && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.city?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">State</label>
+                    <input
+                      {...register("state")}
+                      value={formValues.state}
+                      onChange={onInputChange}
+                      name="state"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.state && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.state?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="p-2">
+                    <label className="inline-block text-right w-24">Zip</label>
+                    <input
+                      {...register("zip")}
+                      value={formValues.zip}
+                      onChange={onInputChange}
+                      name="zip"
+                      type="text"
+                      className="border-2 rounded p-1 mx-2"
+                    />
+                    {errors.zip && (
+                      <p className="text-red-600 text-xs m-2">
+                        {errors.zip?.message}
+                      </p>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            </table>
             <Button text="Add Contact" className="my-4" onClick={() => {}} />
             <Button
               text="Home"
