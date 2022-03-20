55e9ef5e3bb484faef103c65de1f33ed58be22f1
meloegel <meloegel@gmail.com>
2022-03-19 20:28:50 -0400

    added simple header

---
+++ src/common/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header():JSX.Element {
+    return (
+        <div className="py=.5">
+            <h3 className="text-left text-lg">SupLoad</h3>
+        </div>
+    )
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../account/Login";
 import Register from "../account/Register";
+import Header from "../common/Header";
 import AddContact from "../pages/AddContact";
 import Home from "../pages/Home";
 
 export default function Router(): JSX.Element {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
