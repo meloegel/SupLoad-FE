0c477b038b221a432a3f31e6ad8201a1d88dfe40
meloegel <meloegel@gmail.com>
2022-04-11 23:23:57 -0400

    added useFetch to myContact

--- src/pages/MyContact.tsx
+++ src/pages/MyContact.tsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Contact, { ContactType } from "../common/Contact";
+import useFetch from "../hooks/useFetch";
 
 export default function MyContact(): JSX.Element {
   const [userContact, setUserContact] = useState<ContactType>();
+  const [request, data, statusCode] = useFetch<any>();
+  const username = window.localStorage.getItem("username");
+
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    request(`http://localhost:8080/${username}`, {
+      method: "GET",
+      headers: headers,
+    });
+    if (data) {
+      setUserContact(data);
+    }
+    if (statusCode === 400) {
+      console.log("Oh No!");
+    }
+  }, [data, userContact, username, request, statusCode]);
+
   return (
     <div>
       {userContact !== undefined ? (
@@ -16,7 +36,9 @@ export default function MyContact(): JSX.Element {
           zip={userContact.zip}
           phone={userContact.phone}
         />
-      ) : null}
+      ) : (
+        <p>You have not published your own contact with username: {username}</p>
+      )}
     </div>
   );
 }
