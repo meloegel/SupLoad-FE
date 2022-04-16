e589ef5fd1da4eaebb2ba5fb99f30f4e849f9aee
meloegel <meloegel@gmail.com>
2022-04-15 23:46:37 -0400

    added delete my contact option

--- src/common/Contact.tsx
+++ src/common/Contact.tsx
@@ -14,38 +14,40 @@ export default function Contact(props: ContactType): JSX.Element {
     <div className="w-5/12 mx-auto m-2 bg-slate-300 border-2 p-4">
       <h2 className="text-center text-3xl">Contact</h2>
       <table className="m-auto text-2xl">
-        <tr>
-          <td>
-            <h3 className="px-4 py-1 mx-2">First Name: {props.firstname}</h3>
-          </td>
-          <td>
-            <h3 className="px-4 py-1 mx-2">Last Name: {props.lastname}</h3>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h3 className="px-4 py-1 mx-2">Email: {props.email}</h3>
-          </td>
-          <td>
-            <h3 className="px-4 py-1 mx-2">Phone: {props.phone}</h3>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h4 className="px-4 py-1 mx-2">Street: {props.street}</h4>
-          </td>
-          <td>
-            <h4 className="px-4 py-1 mx-2">City: {props.city}</h4>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h4 className="px-4 py-1 mx-2">State: {props.state}</h4>
-          </td>
-          <td>
-            <h4 className="px-4 py-1 mx-2">Zip: {props.zip}</h4>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>
+              <h3 className="px-4 py-1 mx-2">First Name: {props.firstname}</h3>
+            </td>
+            <td>
+              <h3 className="px-4 py-1 mx-2">Last Name: {props.lastname}</h3>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3 className="px-4 py-1 mx-2">Email: {props.email}</h3>
+            </td>
+            <td>
+              <h3 className="px-4 py-1 mx-2">Phone: {props.phone}</h3>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h4 className="px-4 py-1 mx-2">Street: {props.street}</h4>
+            </td>
+            <td>
+              <h4 className="px-4 py-1 mx-2">City: {props.city}</h4>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h4 className="px-4 py-1 mx-2">State: {props.state}</h4>
+            </td>
+            <td>
+              <h4 className="px-4 py-1 mx-2">Zip: {props.zip}</h4>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
--- src/pages/MyContact.tsx
+++ src/pages/MyContact.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import internal from "stream";
 import Button from "../common/Button";
 import Contact, { ContactType } from "../common/Contact";
 import useFetch from "../hooks/useFetch";
 
+type ContactInfo = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: number;
+  phone: string;
+  contactid: number;
+};
+
 export default function MyContact(): JSX.Element {
   const navigate = useNavigate();
   const [userContact, setUserContact] = useState<ContactType>();
-  const [request, data, statusCode] = useFetch<any>();
+  const [contactId, setContactId] = useState<number>();
+  const [request, data, statusCode] = useFetch<ContactInfo>();
   const username = window.localStorage.getItem("username");
 
   useEffect(() => {
@@ -23,12 +37,25 @@ export default function MyContact(): JSX.Element {
   useEffect(() => {
     if (data) {
       setUserContact(data);
+      setContactId(data.contactid);
       console.log(data);
     }
     if (statusCode === 400) {
       console.log("Oh No!");
     }
   }, [statusCode, data]);
+
+  const deleteContact = () => {
+    console.log(contactId)
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    request(`http://localhost:8080/contact/${contactId}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    console.log("fired");
+  };
 
   return (
     <div>
@@ -43,16 +70,27 @@ export default function MyContact(): JSX.Element {
       </div>
       <hr className="border-bottom-2 w-1/4 m-auto my-4 border-white" />
       {userContact !== undefined ? (
-        <Contact
-          firstname={userContact.firstname}
-          lastname={userContact.lastname}
-          email={userContact.email}
-          street={userContact.street}
-          city={userContact.city}
-          state={userContact.state}
-          zip={userContact.zip}
-          phone={userContact.phone}
-        />
+        <>
+          <Contact
+            firstname={userContact.firstname}
+            lastname={userContact.lastname}
+            email={userContact.email}
+            street={userContact.street}
+            city={userContact.city}
+            state={userContact.state}
+            zip={userContact.zip}
+            phone={userContact.phone}
+          />
+          <div className="text-center">
+            <Button
+              className="text-slate-200 ml-2"
+              text="Delete My Contact"
+              onClick={() => {
+                deleteContact();
+              }}
+            />
+          </div>
+        </>
       ) : (
         <div className="text-center">
           <p>
