4768b383ba9b5c1d2a6b97b6fcd6edf57af0b426
meloegel <meloegel@gmail.com>
2022-05-03 21:49:38 -0400

    updated header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,7 +1,8 @@
 export default function Header():JSX.Element {
     return (
-        <div className="py=.5">
+        <div className="py=.5 flex">
             <h3 className="text-left text-xl px-2 text-slate-200">SupLoad</h3>
+            <h3 className="text-left text-xl px-2 text-slate-200">Mark Loegel</h3>
         </div>
     )
 }
