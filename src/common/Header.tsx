54b2f4c67e0acf826ec7819332e7cc872cd39aa9
meloegel <meloegel@gmail.com>
2022-05-04 21:42:42 -0400

    added missing header stlye

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header():JSX.Element {
     return (
-        <div className="py=.5 flex">
+        <div className="py=.5 flex justify-between">
             <h3 className="text-left text-xl px-2 text-slate-200">SupLoad</h3>
             <h3 className="text-left text-xl px-2 text-slate-200">Mark Loegel</h3>
         </div>
