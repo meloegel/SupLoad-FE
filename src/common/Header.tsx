0586e7aa7c569bb9133cfe9580263e60cbe580ea
meloegel <meloegel@gmail.com>
2022-05-07 22:28:23 -0400

    updated header spacing

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header():JSX.Element {
     return (
         <div className="py=.5 flex justify-between">
-            <h3 className="text-left text-xl px-2 text-slate-200">SupLoad</h3>
-            <h3 className="text-left text-xl px-2 text-slate-200">Mark Loegel</h3>
+            <h3 className="text-left text-xl px-4 py-2 text-slate-200">SupLoad</h3>
+            <h3 className="text-left text-xl px-4 py-2 text-slate-200">Mark Loegel</h3>
         </div>
     )
 }
